1f1a6477173b6cac4f00e659af9df3af1a29a540
OnuxSenpai <asasolnusko22210@gmail.com>
2025-06-11 15:15:49 +0300

    Add files via upload

---
+++ classz_1_1/classz_1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Numbers
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите переменную a:");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите переменную b:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        Console.WriteLine($"Произошла смена переменных. Переменная a = {a}, переменная b = {b}");
+    }
+}
