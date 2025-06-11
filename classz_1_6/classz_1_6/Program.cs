3c4d1454d6a0791aca37e58572feacf1e58c889c
OnuxSenpai <asasolnusko22210@gmail.com>
2025-06-11 21:39:22 +0300

    Add files via upload

---
+++ classz_1_6/classz_1_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Numbers
+{
+    public static void Main()
+    {
+        double ooot = 10;
+        double dooo = 15;
+        double shag = 0.5;
+
+        // Выводим заголовок таблицы
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("| Таблица значений функции |");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("|  x   |f(x)=sin(x^2)*ln(x)|");
+        Console.WriteLine("----------------------------");
+
+        for (double x = ooot; x <= dooo; x += shag)
+        {
+            double fx = Math.Sin(x * x) * Math.Log(x);
+            Console.WriteLine($"| {x,4:F1} | {fx,16:F10}  |");
+        }
+        Console.WriteLine("----------------------------");
+    }
+}
