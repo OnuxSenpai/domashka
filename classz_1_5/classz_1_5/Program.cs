8d8f585eeae7c1a5b270d4526984d8fa6080aea1
OnuxSenpai <asasolnusko22210@gmail.com>
2025-06-11 21:02:09 +0300

    Add files via upload

---
+++ classz_1_5/classz_1_5/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Numbers
+{
+    public static void Main()
+    {
+        var otv = (Math.Sin(5) + Math.Pow(1.75, 2)) / (3 * Math.Exp(Math.Cos(7)));
+
+        Console.WriteLine(otv);
+    }
+}
