c8de1ab7eef3aeb458912661f8d817d19f74c1cd
OnuxSenpai <asasolnusko22210@gmail.com>
2025-06-11 03:12:49 +0300

    Add files via upload

---
+++ dz_1_1/dz_1_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class Numbers
+{
+    public static void Main()
+    {
+        // обработка введённого числа
+        Console.WriteLine("Введите число n:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 2; i <= n; i++)
+        {
+            bool danet = true; // булка воспринимает тру и фолс
+
+            for (int j = 2; j <= Math.Sqrt(i); j++)
+            {
+                if (i % j == 0) // Проверка на простое число
+                {
+                    danet = false; // Если нет, то фолс
+                    break; // Выход из цикла для j
+                }
+            }
+
+            if (danet == true) // Если тру, то число простое
+            {
+                Console.WriteLine(i); // Вывод числа на консоль
+            }
+        }
+    }
+}
