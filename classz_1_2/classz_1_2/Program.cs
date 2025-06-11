f8224ab1b302739c57dd889125814136ad48943d
OnuxSenpai <asasolnusko22210@gmail.com>
2025-06-11 17:03:42 +0300

    Add files via upload

---
+++ classz_1_2/classz_1_2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+public class Numbers
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        string str = Console.ReadLine();
+        str = String.Concat(str.OrderBy(c => c));
+        Console.WriteLine($"Преобразование в наименьшее число: {str}");
+    }
+}
