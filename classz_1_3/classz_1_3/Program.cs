4520d18ae52628acca01b366714aa6f40525c18d
OnuxSenpai <asasolnusko22210@gmail.com>
2025-06-11 18:56:33 +0300

    Add files via upload

---
+++ classz_1_3/classz_1_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Numbers
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите число n:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int num = n;
+
+        while (num > 0)
+        {
+            n = 1;
+            for (int i = num; i > 0; i--)
+            {
+                n *= i;
+            }
+            Console.WriteLine("Factorial of {0}! = {1}\n", num, n);
+            num--;
+        }
+    }
+}
