a4756ec7f7ab7227fcb18d4a0c37e9583642eba5
OnuxSenpai <asasolnusko22210@gmail.com>
2025-06-11 23:02:13 +0300

    Add files via upload

---
+++ classz_1_7/classz_1_7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Numbers
+{
+    public static void Main()
+    {
+        Console.Write("Введите вещественное число a: ");
+        string anum = Console.ReadLine();
+        double a = double.Parse(anum);
+
+        Console.Write("Введите знак арифметической операции: ");
+        string znak = Console.ReadLine();
+
+        Console.Write("Введите вещественное число b: ");
+        string bnum = Console.ReadLine();
+        double b = double.Parse(bnum);
+
+        if (znak == "+")
+        {
+            Console.Write(a+b);
+        }    
+        if (znak == "-")
+        {
+            Console.Write(a-b);
+        }
+        if (znak == "*")
+        {
+            Console.Write(a*b);
+        }
+        if (znak == "/")
+        {
+            Console.Write(a/b);
+        }
+        if (znak == "^")
+        {
+            Console.WriteLine(Math.Pow(a, b));
+        }
+    }
+}
