06f7f47c681e0794e25770580c62e699bf52abb5
OnuxSenpai <asasolnusko22210@gmail.com>
2025-06-12 14:32:07 +0300

    Add files via upload

---
+++ classz_1_9/classz_1_9/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+public class Numbers
+{
+    public static void Main()
+    {
+        Console.Write("Введите вещественное число: ");
+        string anum = Console.ReadLine();
+        double a = double.Parse(anum);
+
+        Console.Write("Введите единицу измерений, которую хотите поменять: ");
+        string znak1 = Console.ReadLine();
+
+        Console.Write("Введите единицу измерений, на которую вы хотите поменять: ");
+        string znak2 = Console.ReadLine();
+
+        if (znak1 == "т")
+        {
+            if (znak2 == "ц")
+            {
+                a = a * 10;
+            }
+            if (znak2 == "к")
+            {
+                a = a * 1000;
+            }
+            if (znak2 == "г")
+            {
+                a = a * 1000000;
+            }
+        }
+        if (znak1 == "ц")
+        {
+            if (znak2 == "т")
+            {
+                a = a / 10;
+            }
+            if (znak2 == "к")
+            {
+                a = a * 100;
+            }
+            if (znak2 == "г")
+            {
+                a = a * 100000;
+            }
+        }
+        if (znak1 == "к")
+        {
+            if (znak2 == "т")
+            {
+                a = a / 1000;
+            }
+            if (znak2 == "ц")
+            {
+                a = a / 100;
+            }
+            if (znak2 == "г")
+            {
+                a = a * 1000;
+            }
+        }
+        if (znak1 == "г")
+        {
+            if (znak2 == "т")
+            {
+                a = a / 1000000;
+            }
+            if (znak2 == "ц")
+            {
+                a = a / 100000;
+            }
+            if (znak2 == "к")
+            {
+                a = a / 1000;
+            }
+        }
+        Console.WriteLine(a);
+    }
+}
