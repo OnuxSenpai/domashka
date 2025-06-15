58ffa6e6d5a329114f8c162105d2ea51a604221c
OnuxSenpai <asasolnusko22210@gmail.com>
2025-06-15 17:19:03 +0300

    Add files via upload

---
+++ dz_3_2/dz_3_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public class Okryzhnost
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите радиус окружности: ");
+        int rad = Convert.ToInt32(Console.ReadLine());
+
+        char star = '*';
+
+        // проход по столбцам
+        for (int y = -rad; y <= rad; y++)
+        {
+            // проход по строкам
+            for (int x = -rad; x <= rad; x++)
+            {
+                // грубо говоря рассчет гипотенузы
+                double gip = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+
+                // если погрешность меж гипотенузой и радиусом не велика, то печатаем звезду, иначе пробел
+                if (Math.Abs(gip - rad) < 0.2)
+                {
+                    Console.Write(star);
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            // переход на новую строку
+            Console.WriteLine();
+        }
+    }
+}
