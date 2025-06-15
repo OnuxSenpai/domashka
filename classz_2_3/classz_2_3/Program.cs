8e4d4a66f0c1446ab09e4fbc34479654940f2f84
OnuxSenpai <asasolnusko22210@gmail.com>
2025-06-15 08:49:21 +0300

    Add files via upload

---
+++ classz_2_3/classz_2_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Gipoteza
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите число n:");
+        int n = int.Parse(Console.ReadLine());
+
+        int steps = Steps(n);
+        Console.WriteLine($"Число замен, приводящих до 1: {steps}");
+    }
+    public static int Steps(int n)
+    {
+        int steps = 0;
+        while (n != 1)
+        {
+            if (n % 2 == 0) // проверка на четность
+            {
+                n /= 2;
+            }
+            else
+            {
+                n = 3 * n + 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
